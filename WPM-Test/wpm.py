9549b436c01076f3a201ca86a1bfb9444405e090
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-05-13 14:41:20 -0600

    Update wpm.py

--- WPM-Test/wpm.py
+++ WPM-Test/wpm.py
@@ -69,7 +69,6 @@ def wpm_test(stdscr) :
             current_chars.append(key) 
        
 
-
 def main(stdscr) :
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
