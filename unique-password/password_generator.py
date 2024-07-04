3e3bf53550d9a169868e355e329282978771d846
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-07-04 08:27:19 -0600

    Update password_generator.py

--- unique-password/password_generator.py
+++ unique-password/password_generator.py
@@ -1,7 +1,6 @@
 """
     Dynamically Generated 
           Unique Password
-
         -- user can set length of password (min 7 chars)
         -- user selects if they want it to contain 
             special characters & numbers
@@ -12,42 +11,37 @@
 """
 import random
 import string
+
 print()
 print('Welcome to Unique Password')
 print('Creating dynamically generated passwords. \n')
+
 def generate_password(min_length, numbers=True, special_characters=True) :
     letters = string.ascii_letters
     digits = string.digits
     special = string.punctuation   
     characters = letters  
-
     if numbers :
         characters += digits       
     if special_characters :
         characters += special
-
     pwd = ''
     meets_criteria = False 
     has_number = False
     has_special = False
-
     while not meets_criteria or len(pwd) < min_length :
         new_char = random.choice(characters)
         pwd += new_char
         if new_char in digits :
-            has_number = True
-        
+            has_number = True     
         elif new_char in special :
             has_special = True
-      
         meets_criteria = True
         if numbers :
             meets_criteria = has_number
         if special_characters :
-            meets_criteria = meets_criteria and has_special
-    
+            meets_criteria = meets_criteria and has_special  
     return pwd    
-
 
 def create_pwd() :
     create = input('Would you like to generate an new password: (y/n)  ').lower() == 'y'
