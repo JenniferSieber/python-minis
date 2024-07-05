3905cd60d811cebc6ad2e6ec6e7770b0efb2c96a
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-07-05 09:15:55 -0600

    Update auto-file-backup.py

--- auto-backup-files/auto-file-backup.py
+++ auto-backup-files/auto-file-backup.py
@@ -2,10 +2,8 @@
     Automated File Backups
     pip install schedule
     use: schedule, shutil, os, time, datetime
-
     Schedule daily backup for specific folder on you your harddrive 
     and save that backup with file name by date.
-
 """
 import os
 import shutil
@@ -19,15 +17,12 @@ destination_dir = "C:/User/_USERNAME_/Desktop/Backups"                          
 def copy_folder_to_directory(source, dest) :
     today = datetime.date.today()
     dest_dir = os.path.join(dest, str(today))
-
     try :
         shutil.copytree(source, dest_dir)
         print(f'Folder copied to: {dest_dir}')
     except :
         print(f'Folder already exists in: {dest}')
-
 schedule.every().day.at('11:14').do(lambda: copy_folder_to_directory(source_dir, destination_dir))         # specific to your options
-
 while True :
     schedule.run_pending()
     time.sleep(60)
